635b0e0b0beae44454eec318169f03ef1411527b
Cody Anderson <cody123anderson@gmail.com>
2020-04-02 15:36:22 -0600

    golf

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          "Golf is the best sport!" - Russell Maxfield
         </p>
         <a
           className="App-link"
